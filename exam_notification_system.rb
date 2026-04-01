5151b7dfd06393238f471234d4f4bbca191e836c
İsmail Hakkı Turan <147559619+ismailhakkituran@users.noreply.github.com>
2026-04-01 10:22:55 +0300

    Template structure

--- exam_notification_system.rb
+++ exam_notification_system.rb
@@ -1,1 +1,26 @@
+require blablabla
 
+
+
+class StudentDirectory
+  attr_reader :students_map
+end
+
+
+class CourseRosterShuffler
+  attr_reader :courses, :room_capacities
+end
+
+class Exam
+  attr_accessor :course_name, :date, :time
+end
+
+class ExamSchedule
+  attr_reader :exams
+end
+
+class NotificationService
+end
+
+class ExamTriggerRunner
+end
