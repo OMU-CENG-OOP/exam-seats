d9cecb51e00b54215d39b6945b5b863ff240bb69
Meryem <meyremsena53@gmail.com>
2026-04-29 08:35:16 +0300

    examschedule yazıldı

--- lib/examschedule.rb
+++ lib/examschedule.rb
@@ -14,19 +14,32 @@ class ExamSchedule
 
   def load_from_yml(file_path)
     @source_path = file_path
+    data=YAML.load_file(file_path) 
+
+    rows = raw_exam_rows(data) 
+    rows.each do |row|
+      yeni_sinav = build_exam(row)
+
+      @exams << yeni_sinav
+    end
+
     # TODO: YAML dosyasini okuyup Exam nesneleri uret.
     # Beklenen akisin iskeleti:
     # 1. Dosyayi oku
     # 2. Sinav satirlarini parse et
     # 3. Her kayittan bir Exam nesnesi olustur
     # 4. @exams dizisine ata
-    raise NotImplementedError, "#{self.class}#load_from_yml ogrenci tarafindan doldurulacak."
+    #raise NotImplementedError, "#{self.class}#load_from_yml ogrenci tarafindan doldurulacak."
   end
 
   def due_exams(current_time)
+    #
     # Runner bu metodu cagirir.
     # Bu metod, verilen anda bildirimi tetiklenmesi gereken Exam nesnelerini donmelidir.
-    raise NotImplementedError, "#{self.class}#due_exams ogrenci tarafindan doldurulacak."
+    # raise NotImplementedError, "#{self.class}#due_exams ogrenci tarafindan doldurulacak."
+    @exams.select do |exam|
+      exam.trigger_time <= current_time
+    end 
   end
 
   def add_exam(exam)
@@ -39,7 +52,7 @@ class ExamSchedule
     load_from_yml(source_path)
   end
 
-  def empty?
+  def empty?  
     exams.empty?
   end
 
@@ -47,11 +60,12 @@ class ExamSchedule
 
   def build_exam(exam_row)
     # TODO: exam_row icinden gerekli alanlari alip Exam.new(...) cagir.
-    raise NotImplementedError, "#{self.class}#build_exam ogrenci tarafindan doldurulacak."
+    #raise NotImplementedError, "#{self.class}#build_exam ogrenci tarafindan doldurulacak."
+    Exam.new(exam_row['course_name'], exam_row['date'], exam_row['time'], exam_row['notification_offset_minutes'])
   end
 
-  def raw_exam_rows(yml_data)
+  def raw_exam_rows(yml_data) # veriyi ayıklama
     # TODO: YAML yapisindan sinav satirlarini cikart.
-    raise NotImplementedError, "#{self.class}#raw_exam_rows ogrenci tarafindan doldurulacak."
-  end
+    #raise NotImplementedError, "#{self.class}#raw_exam_rows ogrenci tarafindan doldurulacak."
+    yml_data['exams'] || [] 
 end
